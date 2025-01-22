bba1f583f32ad4683904966990693b20d4f15671
SamuelOmodele <samuelomodele19@gmail.com>
2025-01-22 03:11:34 +0100

    modified footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,38 +3,48 @@ import { FaFacebook } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import styles from './footer.module.css'
+import { FaPhoneAlt } from "react-icons/fa";
+import { IoIosMail } from "react-icons/io";
+import { IoLocationSharp } from "react-icons/io5";
 
 
 const Footer = () => {
-  return (
-    <div className={styles['footer-section']}>
-        <div className={styles['upper-section']}>
-            <div className={styles['left']}>
-                <img src="/BK_logo.png" alt="logo" />
-                <h4>BK Products</h4>
-                <p>Experience innovation with BK</p>
+    return (
+        <div className={styles['footer-section']}>
+            <div className={styles['upper-section']}>
+                <div className={styles['left']}>
+                    <img src="/BK_logo.png" alt="logo" />
+                    <h4>BK Products</h4>
+                    <p>Experience innovation with BK</p>
+                </div>
+                <div className={styles['right']}>
+                    <div className={styles['links']}>
+                        <p>Contact</p>
+                        <p><FaPhoneAlt size={18} /> 08012345678</p>
+                        <p><IoIosMail size={18} /> bkproducts@gmail.com</p>
+                        <p><IoLocationSharp size={18} /> Lagos, Nigeria</p>
+                    </div>
+                    <div className={styles['links']}>
+                        <p>Quick Links</p>
+                        <p>Home</p>
+                        <p>About</p>
+                        <p>Shop</p>
+                        <p>Contact</p>
+                    </div>
+
+                    <div className={styles['links']}>
+                        <p>Social Media</p>
+                        <p><FaFacebook size={22} /> Facebook</p>
+                        <p><FaInstagram size={22} /> Instagram</p>
+                        <p><FaXTwitter size={22} />Twitter</p>
+                    </div>
+                </div>
             </div>
-            <div className={styles['right']}>
-                <div className={styles['links']}>
-                    <p>Quick Links</p>
-                    <p>Home</p>
-                    <p>About</p>
-                    <p>Shop</p>
-                    <p>Contact</p>
-                </div>
-                <div className={styles['links']}>
-                    <p>Social Media</p>
-                    <p><FaFacebook size={22} /></p>
-                    <p><FaInstagram size={22}/></p>
-                    <p><FaXTwitter size={22}/></p>
-                </div>
+            <div className={styles['lower-section']}>
+                &copy; 2025 BK Products. All Rights Reserved. | Designed by BK Team
             </div>
         </div>
-        <div className={styles['lower-section']}>
-            &copy; 2025 BK Products. All Rights Reserved. | Designed by BK Team
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Footer
